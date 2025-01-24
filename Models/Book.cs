c2510ca4d8aad7572c29ca258578cf4516685044
ombhagat256 <152877124+ombhagat256@users.noreply.github.com>
2025-01-24 09:49:01 +0530

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryApi.Models
+{
+    // Models/Book.cs    
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string ISBN { get; set; }
+            public bool Available { get; set; }
+        }
+
+
+}
